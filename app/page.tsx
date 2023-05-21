5b1ca7f7b7cf471e1017464b3349adae49ce06bc
ktkalpha <ktkalpha@naver.com>
2023-05-21 10:11:05 +0900

    fix wtf

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import { FaDiscord } from "react-icons/fa";
 
 export default function Home() {
-<<<<<<< HEAD
-  return <main></main>; //TODO: 뭐라도 넣기
-=======
   return (
     <main className="flex justify-center items-center w-full">
       <FaDiscord size="24rem" color="#474b52"></FaDiscord>
     </main>
   );
->>>>>>> d1f03f0 (add feature)
 }
